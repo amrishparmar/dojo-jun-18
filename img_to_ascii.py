23cde19aa30718855ae45d70437ea6d8dcb13b76
Adam Collard <adam.collard@canonical.com>
2018-06-07 20:51:02 +0100

    Look up ascii character, add scale factor

--- img_to_ascii.py
+++ img_to_ascii.py
@@ -3,6 +3,7 @@ import argparse
 from collections import defaultdict
 import string
 import sys
+from random import choice
 
 from PIL import Image, ImageFont, ImageDraw
 
@@ -10,9 +11,13 @@ from PIL import Image, ImageFont, ImageDraw
 def load_image_as_grayscale(filename):
     return Image.open(filename).convert('LA')
 
-def resize_image(img):
+
+def resize_image(img, scale_factor=8):
     width, height = img.size
-    return img.resize((width, height//2), resample=Image.BICUBIC)
+    return img.resize(
+        (width // scale_factor, height // (2 * scale_factor)),
+        resample=Image.BICUBIC)
+
 
 def get_luminance(characters):
     font = ImageFont.truetype("/usr/share/fonts/truetype/ubuntu/UbuntuMono-R.ttf", 12)
@@ -33,26 +38,38 @@ def normalize_luminance(luminance):
     normalized_luminance = {}
     for key, values in luminance.items():
         normalized_key = round((key * 255) / max_key)
-        normalized_luminance[normalized_key] = values
+        normalized_luminance[255 - normalized_key] = values
     return normalized_luminance
 
-def convert_to_ascii(image_filename):
-    pass
+
+def get_closest(mapping, value):
+    return mapping.get(value, mapping[min(mapping.keys(), key=lambda k: abs(k-value))])
+
+
+def convert_to_ascii(image, luminance):
+    out = ""
+    for n, pixel in enumerate(image.getdata(band=0)):
+        closest = get_closest(luminance, pixel)
+        if n != 0 and n % image.width == 0:
+            out += "\n"
+        out += choice(closest)
+
+    return out
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('image')
+    parser.add_argument('--scale-factor', type=int)
     args = parser.parse_args(sys.argv[1:])
 
     img = load_image_as_grayscale(args.image)
-    img = resize_image(img)
-    img.save('output.png')
+    img = resize_image(img, args.scale_factor)
+    # img.save('output.png')
     luminance = get_luminance(string.printable[:-5])
     luminance = normalize_luminance(luminance)
-    from pprint import pprint
-    pprint(dict(luminance))
-    ascii_ = convert_to_ascii(args.image)
+
+    ascii_ = convert_to_ascii(img, luminance)
     print(ascii_)
 
 
