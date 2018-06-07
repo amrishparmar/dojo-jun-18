fc08a35279d9565373f06854716fad635316bfc8
Amrish Parmar <amrish.parmar@me.com>
2018-06-07 20:27:36 +0100

    changed tabs to spaces

--- img_to_ascii.py
+++ img_to_ascii.py
@@ -5,22 +5,22 @@ from PIL.Image import BICUBIC
 
 
 def load_image_as_grayscale(filename):
-	return Image.open(filename).convert('LA')
+    return Image.open(filename).convert('LA')
 
 
 def resize_image(img):
-	width, height = img.size
-	return img.resize((width, height//2), resample=BICUBIC)
+    width, height = img.size
+    return img.resize((width, height//2), resample=BICUBIC)
 
 
 def main():
-	img = load_image_as_grayscale(sys.argv[1])
-	img = resize_image(img)
+    img = load_image_as_grayscale(sys.argv[1])
+    img = resize_image(img)
 
-	# do the processing here
+    # do the processing here
 
-	img.save('output.png')
+    img.save('output.png')
 
 
 if __name__ == '__main__':
-	main()
+    main()
